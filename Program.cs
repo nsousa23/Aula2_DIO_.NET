7e4ae7eeff4ee082ad6221ec6b4434f9149d9c9a
nsousa23 <nil.van23@hotmail.com>
2022-05-26 00:28:12 -0300

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula_2
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int idade;
+
+           Console.WriteLine("Digite a sua idade:");
+           idade =int.Parse(Console.ReadLine());
+
+              if (idade >= 18)
+              {
+                  Console.WriteLine("Você é maior de idade");
+
+              }
+              else {
+                  Console.WriteLine("Você é menor de idade");
+              }
+        }
+    }
+}
